fdec342683d46f412ee79ff6b3cea92edc66c6ed
RyanGomes338 <ryangomes338@gmail.com>
2024-12-11 14:00:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,23 +8,26 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 driver.get("https://g1.globo.com/")
 
-search_box = driver.find_element(By.NAME, "q")
+search_box = driver.find_element(By.ID, "busca-campo")
 search_box.send_keys("Inteligência Artificial")
 search_box.submit()
 
 time.sleep(5) 
 
-noticias = driver.find_elements(By.CSS_SELECTOR, "feed-post-body-title")
-datas = driver.find_elements(By.CSS_SELECTOR, "feed-post-datetime")
 
-with open("noticias.txt", "w", encoding="utf-8") as file: 
+noticias = driver.find_elements(By.CLASS_NAME, "widget--info__title")
+
+datas = driver.find_elements(By.CLASS_NAME, "widget--info__meta")
+
+
+with open("noticias.txt", "a") as news_file: 
     for noticia, data in zip(noticias, datas):
         try: 
-            titulo = noticias.text 
+            titulo = noticia.text 
             data_publicacao = data.text
-            file.write(f"Título: {titulo}\n") 
-            file.write(f"Data de Publicação: {data_publicacao}\n") 
-            file.write("-" * 50 + "\n")
+            news_file.write(f"Título: {titulo}\n") 
+            news_file.write(f"Data de Publicação: {data_publicacao}\n") 
+            news_file.write("-" * 50 + "\n")
         except: 
             continue
 
